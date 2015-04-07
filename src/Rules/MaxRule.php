679f38195b36b2c0d06af7f81edcc02359a62d73
Alex Garrett <alex@phpacademy.org>
2015-04-07 13:01:41 +0100

    Updated min and max rule to fix bug

--- src/Rules/MaxRule.php
+++ src/Rules/MaxRule.php
@@ -8,13 +8,13 @@ class MaxRule implements RuleContract
 {
     public function run($value, $input, $args)
     {
-        if (is_numeric($value)) {
+        $number = isset($args[1]) && $args[1] === 'number';
+
+        if ($number) {
             return (float) $value <= (float) $args[0];
         }
 
-        if (is_string($value)) {
-            return mb_strlen($value) <= (int) $args[0];
-        }
+        return mb_strlen($value) <= (int) $args[0];
     }
 
     public function error()
