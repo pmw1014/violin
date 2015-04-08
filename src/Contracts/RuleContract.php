3a12ac6e68762e36e7eac5207450d6fe734cc11a
Alex Garrett <alex@phpacademy.org>
2015-04-08 12:18:36 +0100

    Removed whitespace from comments

--- src/Contracts/RuleContract.php
+++ src/Contracts/RuleContract.php
@@ -7,18 +7,18 @@ interface RuleContract
     /**
      * Runs the rule to check validity. Returning false fails
      * the check and returning true passes the check.
-     * 
+     *
      * @param  mixed $value
      * @param  array $input
      * @param  array $args
-     * 
+     *
      * @return bool
      */
     public function run($value, $input, $args);
 
     /**
      * The error given if the rule fails.
-     * 
+     *
      * @return string
      */
     public function error();
@@ -26,7 +26,7 @@ interface RuleContract
     /**
      * If the rule can be skipped, if the value given
      * to the validator is not required.
-     * 
+     *
      * @return [type] [description]
      */
     public function canSkip();
