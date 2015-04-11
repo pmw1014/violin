59d5afa0dee358a88ed0601cb6d51a6070feab60
Alex Garrett <alex@phpacademy.org>
2015-04-11 16:51:44 +0100

    Basic example

---
+++ examples/basic.php
@@ -0,0 +1,20 @@
+<?php
+
+require '../vendor/autoload.php';
+
+use Violin\Violin;
+
+$v = new Violin;
+
+$v->validate([
+    'username'  => ['dale', 'required|alpha|min(3)|max(20)'],
+    'email'     => ['dale@codecourse.com', 'required|email'],
+    'password'  => ['ilovecats', 'required'],
+    'password_confirm' => ['ilovecats', 'required|matches(password)']
+]);
+
+if ($v->passes()) {
+    // Passed
+} else {
+    var_dump($v->errors()->all());
+}
